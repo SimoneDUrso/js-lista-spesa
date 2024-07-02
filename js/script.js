833e928383ad62e4a16c22745aa43c04c42430d1
SimoneDUrso <simone.durso98@gmail.com>
2024-07-02 14:32:23 +0200

    [UPD] Created WHILE cycle

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const ingredienti = ["spaghetti", "uova", "pecorino", "guanciale", "pepe"]
 // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
 const lista_spesa = document.getElementById("lista-spesa");
 
+// CICLO L'ARRAY CON UN WHILE
+let i = 0;
+while (i < ingredienti.length) {
+
+
+
+    i++;
+}
