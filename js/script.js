a4851baa9091dd86cc63f947db8165a45293426e
SimoneDUrso <simone.durso98@gmail.com>
2024-07-02 14:31:45 +0200

    [UPD] I take the element that must contain the shopping list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // LISTA DELLA SPESA
 const ingredienti = ["spaghetti", "uova", "pecorino", "guanciale", "pepe"]
 
+// RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
+const lista_spesa = document.getElementById("lista-spesa");
+
